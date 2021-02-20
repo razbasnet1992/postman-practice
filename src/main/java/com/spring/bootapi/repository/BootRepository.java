54b7b768d09a9036d767944039005f93f6540112
razbasnet1992 <razbasnet1992@users.noreply.github.com>
2021-02-20 17:31:35 -0500

    repository change

--- src/main/java/com/spring/bootapi/repository/BootRepository.java
+++ src/main/java/com/spring/bootapi/repository/BootRepository.java
@@ -3,7 +3,9 @@ package com.spring.bootapi.repository;
 public class BootRepository {
 
     public static void main(String[] args) {
+
         System.out.println("Hi this is sameer pakuwal");
+        System.out.println("Hi this is raaz basnet");
     }
 
 
