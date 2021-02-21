2eddecab3914c4e7f7a0f3ae112c8de2735080af
razbasnet1992 <69375370+razbasnet1992@users.noreply.github.com>
2021-02-20 19:41:32 -0500

    Update BootRepository.java

--- src/main/java/com/spring/bootapi/repository/BootRepository.java
+++ src/main/java/com/spring/bootapi/repository/BootRepository.java
@@ -5,9 +5,8 @@ public class BootRepository {
     public static void main(String[] args) {
 
         System.out.println("Hi this is sameer pakuwal");
-        System.out.println("Hi this is raaz basnet");
-         System.out.println("Hi this is github practice");
          System.out.println("Hi this is develop branch");
+
 
     }
 
